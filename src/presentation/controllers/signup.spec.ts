f66c42b568ab4220a2f42b0061120670547767ef
Douglas Poma <douglaspoma@yahoo.com>
2021-05-30 17:55:12 -0300

    refactor: move EmailValidorStub creation to a factory helper method

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -7,13 +7,16 @@ type SutType = {
   sut: SignUpController
   emailValidatorStub: EmailValidator
 }
-const makeSut = (): SutType => {
+const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
     isValid (email: string): boolean {
       return true
     }
   }
-  const emailValidatorStub = new EmailValidatorStub()
+  return new EmailValidatorStub()
+}
+const makeSut = (): SutType => {
+  const emailValidatorStub = makeEmailValidator()
   const sut = new SignUpController(emailValidatorStub)
   return { sut, emailValidatorStub }
 }
