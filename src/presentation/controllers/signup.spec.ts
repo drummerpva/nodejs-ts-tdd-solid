bfab65841fa9e3bdb840a0ea67f95ff1eac1993a
Douglas Poma <douglaspoma@yahoo.com>
2021-05-30 15:47:34 -0300

    test: ensure SignUPController returns 400 if no name provided

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,16 @@
+import { SignUpController } from './signup'
+import faker from 'faker'
+describe('SignUp Controller', () => {
+  test('Should return 400 if no name is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        email: faker.internet.email,
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
