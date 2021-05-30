11de451e876d81e67eb7f49f30ca03a9b4aa8ad4
Douglas Poma <douglaspoma@yahoo.com>
2021-05-30 17:28:36 -0300

    test: ensure SignUPController calls EmailValidator with correct value

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -74,6 +74,20 @@ describe('SignUp Controller', () => {
     expect(httpResponse?.statusCode).toBe(400)
     expect(httpResponse?.body).toEqual(new MissingParamError('passwordConfirmation'))
   })
+  test('Should return 400 if password not equal passwordConfirmation is provided', () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: faker.name.findName,
+        email: faker.internet.email,
+        password: faker.random.alphaNumeric(8),
+        passwordConfirmation: faker.random.alphaNumeric(9)
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse?.statusCode).toBe(400)
+    expect(httpResponse?.body).toEqual(new InvalidParamError('passwordConfirmation'))
+  })
   test('Should return 400 if invalid email is provided', () => {
     const { sut, emailValidatorStub } = makeSut()
     jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
@@ -90,18 +104,20 @@ describe('SignUp Controller', () => {
     expect(httpResponse?.statusCode).toBe(400)
     expect(httpResponse?.body).toEqual(new InvalidParamError('email'))
   })
-  test('Should return 400 if password not equal passwordConfirmation is provided', () => {
-    const { sut } = makeSut()
+  test('Should call EmailValidator with correct email', () => {
+    const { sut, emailValidatorStub } = makeSut()
+    const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
+    const passFake = faker.internet.password
+    const mailFake = faker.internet.email
     const httpRequest = {
       body: {
         name: faker.name.findName,
-        email: faker.internet.email,
-        password: faker.random.alphaNumeric(8),
-        passwordConfirmation: faker.random.alphaNumeric(9)
+        email: mailFake,
+        password: passFake,
+        passwordConfirmation: passFake
       }
     }
-    const httpResponse = sut.handle(httpRequest)
-    expect(httpResponse?.statusCode).toBe(400)
-    expect(httpResponse?.body).toEqual(new InvalidParamError('passwordConfirmation'))
+    sut.handle(httpRequest)
+    expect(isValidSpy).toHaveBeenCalledWith(mailFake)
   })
 })
