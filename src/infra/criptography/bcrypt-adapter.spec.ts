9fbfa0f84e401f20094902c35d2aaad61966a856
Douglas Poma <douglaspoma@yahoo.com>
2021-06-02 19:38:01 -0300

    test: ensure BcryptAdapter return a hash if sucess

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -18,10 +18,16 @@ describe('BCrypt Adaper', () => {
   })
   test('Should call bcrypt with correct value', async () => {
     const salt = 12
-    const sut = makeSut(12)
+    const sut = makeSut(salt)
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     const passwordFake = faker.internet.password()
     await sut.encrypt(passwordFake)
     expect(hashSpy).toHaveBeenCalledWith(passwordFake, salt)
+  })
+  test('Should return a hash on success', async () => {
+    const salt = 12
+    const sut = makeSut(salt)
+    const hash = await sut.encrypt(faker.internet.password())
+    expect(hash).toBeTruthy()
   })
 })
