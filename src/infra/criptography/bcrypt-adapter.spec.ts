9c5ea59106d0bd49e9d2f0973a149d73b09c0eb6
Douglas Poma <douglaspoma@yahoo.com>
2021-06-02 18:24:15 -0300

    test: ensure BcryptAdapter call bcrypt only once

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -8,6 +8,13 @@ const makeSut = (salt: number | string): BcryptAdapter => {
 }
 
 describe('BCrypt Adaper', () => {
+  test('Should call bcrypt only once', async () => {
+    const sut = makeSut(12)
+    const hashSpy = jest.spyOn(bcrypt, 'hash')
+    const passwordFake = faker.internet.password()
+    await sut.encrypt(passwordFake)
+    expect(hashSpy).toBeCalledTimes(1)
+  })
   test('Should call bcrypt with correct value', async () => {
     const sut = makeSut(12)
     const hashSpy = jest.spyOn(bcrypt, 'hash')
