cf577c1eaaa06ce364cf42817399ba3d2656465f
Douglas Poma <douglaspoma@yahoo.com>
2021-06-02 18:16:59 -0300

    test: ensure BcryptAdapter call bcrypt with correct values

---
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -0,0 +1,18 @@
+import faker from 'faker'
+import bcrypt from 'bcrypt'
+import { BcryptAdapter } from './bcrypt-adapter'
+
+const makeSut = (salt: number | string): BcryptAdapter => {
+  const sut = new BcryptAdapter(salt)
+  return sut
+}
+
+describe('BCrypt Adaper', () => {
+  test('Should call bcrypt with correct value', async () => {
+    const sut = makeSut(12)
+    const hashSpy = jest.spyOn(bcrypt, 'hash')
+    const passwordFake = faker.internet.password()
+    await sut.encrypt(passwordFake)
+    expect(hashSpy).toHaveBeenCalledWith(passwordFake, 12)
+  })
+})
