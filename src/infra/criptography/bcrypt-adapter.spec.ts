ff0d869fe2cf25aa893b37eab05d429eeb402dea
Douglas Poma <douglaspoma@yahoo.com>
2021-06-02 19:32:24 -0300

    refactor: move to variable value of salt

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -9,17 +9,19 @@ const makeSut = (salt: number | string): BcryptAdapter => {
 
 describe('BCrypt Adaper', () => {
   test('Should call bcrypt only once', async () => {
-    const sut = makeSut(12)
+    const salt = 12
+    const sut = makeSut(salt)
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     const passwordFake = faker.internet.password()
     await sut.encrypt(passwordFake)
     expect(hashSpy).toBeCalledTimes(1)
   })
   test('Should call bcrypt with correct value', async () => {
+    const salt = 12
     const sut = makeSut(12)
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     const passwordFake = faker.internet.password()
     await sut.encrypt(passwordFake)
-    expect(hashSpy).toHaveBeenCalledWith(passwordFake, 12)
+    expect(hashSpy).toHaveBeenCalledWith(passwordFake, salt)
   })
 })
