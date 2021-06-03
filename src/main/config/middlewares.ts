9155e7c9a97b2f09a91fadf62bb987ce5b6c06ed
Douglas Poma <douglaspoma@yahoo.com>
2021-06-03 20:19:11 -0300

    refactor: move export middlewares to a single file

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,7 +1,5 @@
 import { Express } from 'express'
-import { bodyParser } from '../middlewares/body-parser'
-import { contentTypeJson } from '../middlewares/content-type'
-import { cors } from '../middlewares/cors'
+import { bodyParser, contentTypeJson, cors } from '../middlewares'
 export default (app: Express): void => {
   app.use(cors)
   app.use(bodyParser)
