a803778ce8a042886b44bc58c75503c3c8af16d9
Douglas Poma <douglaspoma@yahoo.com>
2021-06-04 00:23:07 -0300

    feat: connect to mongodb before app listen

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,2 +1,21 @@
-import app from './config/app'
-app.listen(3000, () => console.log(`Server running att http://localhost:3000`))
+import env from './config/env'
+import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
+
+/* MongoHelper.connect(env.mongoUrl)
+  .then(async () => {
+    const app = (await import('./config/app')).default
+    app.listen(env.port, () =>
+      console.log(`Server running att http://localhost:${env.port}`)
+    )
+  })
+  .catch(console.error) */
+async function init(): Promise<void> {
+  await MongoHelper.connect(env.mongoUrl)
+  const app = (await import('./config/app')).default
+  app.listen(env.port, () =>
+    console.log(`Server running att http://localhost:${env.port}`)
+  )
+}
+
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+init()
